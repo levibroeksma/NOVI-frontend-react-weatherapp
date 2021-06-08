4c529b99703d04fad98e90e463a494fa30e59811
School <School@Levis-oude-bakbeest.local>
2021-06-08 14:08:41 +0200

    solves paragraph 1-4, WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
+import axios from 'axios';
+
+const apiKey = 'dd1a0d338bf333f2c40690b52178ddf0';
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+      setWeatherData(result.data);
+      console.log(result.data)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <>
       <div className="weather-container">
@@ -12,18 +27,19 @@ function App() {
         {/*HEADER -------------------- */}
         <div className="weather-header">
           <SearchBar/>
-
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
-
-            <button type="button">
+            {weatherData && <>
+              <h2>{weatherData.weather[0].description}</h2>
+              <h3> {weatherData.name}</h3>
+              <h1>{weatherData.main.temp}</h1>
+            </>}
+            <button
+                type="button"
+            onClick={fetchData}>
               Haal data op!
             </button>
           </span>
         </div>
-
         {/*CONTENT ------------------ */}
         <div className="weather-content">
           <TabBarMenu/>
